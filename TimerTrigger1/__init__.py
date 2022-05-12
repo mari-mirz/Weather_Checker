bdbc08be1d90e11eb587abe813e86f4629b6f3fb
ripstick420 <94776653+Mariana-Mirzoyan@users.noreply.github.com>
2022-05-12 18:26:16 +1000

    edit

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,19 +1,17 @@
 import datetime
 import logging
-import TimerTrigger1.weather_checker as weather_checker
+from weather_checker import rain
 
 import azure.functions as func
 
-
 def main(mytimer: func.TimerRequest) -> None:
+
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    message = weather_checker.rain
-    print (message)
-    return message
+    rain()
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
