f653b2990deda0fdfc8c665e158a31a326909409
ripstick420 <94776653+Mariana-Mirzoyan@users.noreply.github.com>
2022-05-19 11:21:06 +1000

    commit

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,11 +1,6 @@
 import datetime
 import logging
-<<<<<<< HEAD
-from weather_checker import rain
-
-=======
 from main import rain
->>>>>>> 6eec79cd7bb8e8e3d9aa0c2e72840329bfb5a740
 import azure.functions as func
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -17,9 +12,5 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     rain()
-<<<<<<< HEAD
-
-=======
     
->>>>>>> 6eec79cd7bb8e8e3d9aa0c2e72840329bfb5a740
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
